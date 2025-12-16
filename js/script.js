dd0c8c61abe34500b0925bc6fb6fdcb598280bad
Kanat <kanat2008.kn@gmail.com>
2025-12-17 04:06:39 +0500

    dz

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// let buttonText = "Не корзина";
+
+// document.querySelector(".container__cart").addEventListener("click", function() {
+//     this.classList.toggle("active");
+// });
+
+
+
+// document.querySelector(".container__cart")
+
+// let a;
+// a = "Kana";
+// if (a =="Kanat") {
+//     alert (56465464566);
+// } else {
+//     alert ("false")
+// }
+
+// a = "321"
+
+// let buttonText = "Корзина | 1";
+// let buttonText2 = "Не корзина";
+// document.querySelector(".container__cart").addEventListener("click",function(){
+//     if(this.textContent == buttonText2) {
+//         this.textContent = buttonText;
+//     } else{
+//         this.textContent = buttonText2
+//     }
+// })
+
+let buttonText;
+let buttonText2 = "Не кор";
+document.querySelector(".container__cart").addEventListener("click",function(){
+        buttonText = this.textContent;
+        this.textContent = buttonText2;
+        buttonText2 = buttonText;
+})
